553ab4dd31485e9b9667caa19b383f5f1f13fc41
afreakanist <afreakanist@gmail.com>
2021-05-20 23:01:00 +0600

    feat: Facebook Plugin + styles

---
+++ src/components/FacebookPlugin/FacebookPlugin.js
@@ -0,0 +1,12 @@
+import './FacebookPlugin.css';
+
+const FacebookPlugin = () => (
+  <iframe
+    className="facebook"
+    title="bbbs-facebook-feed"
+    src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBigBrothers.BigSisters.Russia&tabs=timeline&width=420&height=627&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId"
+    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+  />
+);
+
+export default FacebookPlugin;
