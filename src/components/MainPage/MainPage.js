eaa98c2062ec82ffc216605057e59d9466dc486d
afreakanist <afreakanist@gmail.com>
2021-05-21 00:33:09 +0600

    update: questions + fb plugin added to main

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,3 +1,14 @@
+import MainPageSection from '../MainPageSection/MainPageSection';
+import FacebookPlugin from '../FacebookPlugin/FacebookPlugin';
+import QuestionsContainer from '../QuestionsContainer/QuestionsContainer';
+
 export default function MainPage() {
-  return <div>MAIN PAGE</div>;
+  return (
+    <div>
+      <MainPageSection className="mainpage__blocks-col">
+        <FacebookPlugin />
+        <QuestionsContainer place="main" />
+      </MainPageSection>
+    </div>
+  );
 }
