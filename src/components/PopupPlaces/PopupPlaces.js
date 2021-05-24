5abee2f4d9f542b3c8ad0ff1b09adef83f9c2356
afreakanist <afreakanist@gmail.com>
2021-05-24 23:25:13 +0600

    feat: form submit handling

--- src/components/PopupPlaces/PopupPlaces.js
+++ src/components/PopupPlaces/PopupPlaces.js
@@ -1,10 +1,27 @@
 import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import './PopupPlaces.css';
 import PropTypes from 'prop-types';
 import Button from '../Button/Button';
 import Popup from '../Popup/Popup';
 
 const PopupPlaces = ({ isOpen, onClose }) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+
+  const onSubmit = (placeData) => {
+    console.log(placeData);
+    // placeData — то, что соберётся с формы; эти данные и пойдут на сервер
+    reset();
+    onClose();
+  };
+
+  // TO DO: перенести обработку клика по esc и оверлею в компонент Popup
+
   useEffect(() => {
     const handleEscClick = (event) => {
       if (event.key === 'Escape') onClose();
@@ -17,10 +34,8 @@ const PopupPlaces = ({ isOpen, onClose }) => {
     };
   }, [isOpen, onClose]);
 
-  const handleSubmit = () => {};
-
   return (
-    <Popup popupType="popup_type_recommendation" isOpen={isOpen} onClick={onClose}>
+    <Popup popupType="popup_type_recommendation" isOpen={isOpen} onClose={onClose}>
       <div className="recommendation">
         <div className="recommendation__texts">
           <p className="recommendation__text">
@@ -34,60 +49,81 @@ const PopupPlaces = ({ isOpen, onClose }) => {
         <form
           className="popup__form popup__form_type_recommendation"
           name="recommendation"
-          action="#"
-          method="POST"
-          noValidate
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(onSubmit)}
         >
           <div className="popup__inputs">
             <input
+              {...register('title', {
+                required: 'Введите название места',
+                minLength: { value: 2, message: 'Название места должно быть не менее 2 символов' },
+              })}
               type="text"
-              name="name"
-              placeholder="Название"
-              id="name"
-              className="popup__input popup__input_type_name"
-              required
-              minLength="2"
+              placeholder={errors.title ? errors.title.message : 'Название места*'}
+              className={`popup__input popup__input_type_name ${
+                errors.title ? 'popup__input-error' : ''
+              }`}
             />
             <input
-              className="popup__input popup__input_type_url"
+              {...register('link')}
               type="url"
-              placeholder="Сайт"
-              name="date"
+              className={`popup__input popup__input_type_url ${
+                errors.link ? 'popup__input-error' : ''
+              }`}
+              placeholder={errors.link ? errors.link.message : 'Сайт'}
             />
             <input
-              className="popup__input popup__input_type_address"
+              {...register('address', { required: 'Введите адрес' })}
               type="text"
-              placeholder="Адрес*"
-              name="address"
+              className={`popup__input popup__input_type_address ${
+                errors.address ? 'popup__input-error' : ''
+              }`}
+              placeholder={errors.address ? errors.address.message : 'Адрес*'}
             />
             <div className="popup__input popup__input_type_boy">
               <label htmlFor="boy">
-                <input className="custom-radio" type="radio" name="boy" id="boy" />
+                <input
+                  className="custom-radio"
+                  type="radio"
+                  value="Мальчик"
+                  {...register('sex')}
+                  id="boy"
+                />
                 Мальчик
               </label>
             </div>
             <div className="popup__input popup__input_type_girl">
               <label htmlFor="girl">
-                <input className="custom-radio" type="radio" name="girl" id="girl" />
+                <input
+                  className="custom-radio"
+                  type="radio"
+                  value="Девочка"
+                  {...register('sex')}
+                  id="girl"
+                />
                 Девочка
               </label>
             </div>
             <input
-              className="popup__input popup__input_type_age"
+              {...register('age', { required: 'Введите возраст младшего' })}
               type="number"
-              placeholder="Возраст*"
-              name="age"
+              className={`popup__input popup__input_type_age ${
+                errors.age ? 'popup__input-error' : ''
+              }`}
+              placeholder={errors.age ? errors.age.message : 'Возраст*'}
             />
-            <select className="popup__select popup__select_type_relax">
+            <select className="popup__select popup__select_type_relax" {...register('type')}>
               <option>Активный</option>
               <option>Развлекательный</option>
               <option>Познавательный</option>
             </select>
             <textarea
-              className="popup__textarea popup__textarea_type_description"
-              placeholder="Комментарий*"
-              name="description"
+              {...register('description', {
+                required: 'Расскажите о ваших с младшим впечатлениях',
+              })}
+              className={`popup__textarea popup__textarea_type_description ${
+                errors.description ? 'popup__textarea-error' : ''
+              }`}
+              placeholder={errors.description ? errors.description.message : 'Комментарий*'}
             />
             <div className="popup__feedback">
               <Button className="popup__feedback-button" type="button" />
@@ -104,8 +140,6 @@ const PopupPlaces = ({ isOpen, onClose }) => {
     </Popup>
   );
 };
-
-// TO DO: разобраться с обработкой сабмита
 
 PopupPlaces.propTypes = {
   isOpen: PropTypes.bool.isRequired,
