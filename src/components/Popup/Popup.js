b90ff5386490f1a30e6204a98d0847a240b1fe51
afreakanist <afreakanist@gmail.com>
2021-05-25 05:24:54 +0600

    feat: popup closing on overlay / esc click

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,17 +1,46 @@
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './popup.css';
 
-const Popup = ({ popupType, isOpen, children }) => (
-  <div className={`popup ${popupType} ${isOpen ? 'popup_opened' : ''}`}>{children}</div>
-);
+const Popup = ({ popupType, isOpen, onClose, children }) => {
+  const handleOverlayClick = (event) => {
+    if (event.target === event.currentTarget && isOpen) onClose();
+  };
+
+  useEffect(() => {
+    const handleEscClick = (event) => {
+      if (event.key === 'Escape') onClose();
+    };
+
+    document.addEventListener('keydown', handleEscClick);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscClick);
+    };
+  }, [isOpen, onClose]);
+
+  return (
+    <div
+      className={`popup ${popupType} ${isOpen ? 'popup_opened' : ''}`}
+      onMouseDown={handleOverlayClick}
+      role="presentation"
+    >
+      {children}
+    </div>
+  );
+};
 
 Popup.propTypes = {
   popupType: PropTypes.string,
   isOpen: PropTypes.bool,
+  onClose: PropTypes.func,
   children: PropTypes.node.isRequired,
 };
+
 Popup.defaultProps = {
   popupType: '',
   isOpen: false,
+  onClose: () => {},
 };
+
 export default Popup;
