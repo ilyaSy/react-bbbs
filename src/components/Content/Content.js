835db0542505d31f6a91bc03aa2a7d5e946cb3be
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-05-26 05:29:46 +0300

    Fix: resolve conflicts

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -32,7 +32,14 @@ export default function Content({
     <Switch>
       <Route exact path="/">
         <main className="main">
+          {/* </main><main className="content root__section"> */}
           <MainPage mainData={mainData} />
+          {/* <MainPage
+            mainData={mainData}
+            handleCalendarCardClick={handleCalendarCardClick}
+            handleRegisterSubmit={handleRegisterSubmit}
+            handleDeleteEvent={handleDeleteEvent}
+          /> */}
         </main>
       </Route>
 
