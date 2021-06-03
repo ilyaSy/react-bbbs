1d6d523ffb682658ad90ee5a630bcb8302d808d2
Andrey Botygin <dron-i90@yandex.ru>
2021-06-03 19:14:50 +0300

    фигурки в правах фикс

--- src/components/Cards/RightsCard/RightsCard.js
+++ src/components/Cards/RightsCard/RightsCard.js
@@ -3,10 +3,12 @@ import Tag from '../../UI/Tag/Tag';
 
 const ArticleRights = ({ title, tag, figure, color }) => (
   <li className="law__item">
-    <a href="/" className={`law__link law__link_form_${figure} law__link_color_${color}`}>
-      <h3 className="law__heading">{title}</h3>
-      <Tag tagText={tag.name} />
-    </a>
+    <div className="law__wrapper">
+      <a href="/" className={`law__link law__link_form_${figure} law__link_color_${color}`}>
+        <h3 className="law__heading">{title}</h3>
+        <Tag tagText={tag.name} />
+      </a>
+    </div>
   </li>
 );
 
